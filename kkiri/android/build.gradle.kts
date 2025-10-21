2bf2a8288befc4e6942faa0d3cc3fd4ffceecd8f
ehgus2390 <165624422+ehgus2390@users.noreply.github.com>
2025-10-21 10:38:07 +0900

    ddf
    dfds

--- kkiri/android/build.gradle.kts
+++ kkiri/android/build.gradle.kts
@@ -3,6 +3,10 @@ allprojects {
         google()
         mavenCentral()
     }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:8.2.2'
+        classpath 'com.google.gms:google-services:4.4.2'
+    }
 }
 
 val newBuildDir: Directory =
