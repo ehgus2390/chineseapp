b795311b6e51c414c12a1219b7ecb2ad0d8c0834
ehgus2390 <165624422+ehgus2390@users.noreply.github.com>
2025-11-20 22:44:25 +0900

    Handle geofire position parsing in map markers

---
+++ kkiri/lib/constants/interest_options.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class InterestOption {
+  const InterestOption({required this.id, required this.icon});
+
+  final String id;
+  final IconData icon;
+}
+
+const List<InterestOption> kInterestOptions = [
+  InterestOption(id: 'coffee', icon: Icons.coffee),
+  InterestOption(id: 'travel', icon: Icons.flight_takeoff),
+  InterestOption(id: 'hiking', icon: Icons.landscape),
+  InterestOption(id: 'music', icon: Icons.music_note),
+  InterestOption(id: 'gaming', icon: Icons.sports_esports),
+  InterestOption(id: 'art', icon: Icons.brush),
+  InterestOption(id: 'foodie', icon: Icons.ramen_dining),
+  InterestOption(id: 'language', icon: Icons.translate),
+  InterestOption(id: 'kdrama', icon: Icons.live_tv),
+  InterestOption(id: 'fitness', icon: Icons.self_improvement),
+];
+
+const Set<String> kInterestOptionIds = {
+  for (final option in kInterestOptions) option.id,
+};
