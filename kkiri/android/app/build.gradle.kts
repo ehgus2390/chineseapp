9f766a5112ae59ae50c10e1c886c75d4f9c12014
ehgus2390 <165624422+ehgus2390@users.noreply.github.com>
2025-11-20 20:57:12 +0900

    asdasd
    asdasdd

--- kkiri/android/app/build.gradle.kts
+++ kkiri/android/app/build.gradle.kts
@@ -1,67 +1,14 @@
-//plugins {
-//    id("com.android.application")
-//    id("org.jetbrains.kotlin.android")
-//    id("dev.flutter.flutter-gradle-plugin")
-//    id("com.google.gms.google-services")
-//}
-//
-//android {
-//    namespace = "com.ant.company"
-//    compileSdk = flutter.compileSdkVersion
-//    ndkVersion = flutter.ndkVersion
-//
-//    defaultConfig {
-//        applicationId = "com.ant.company"
-//        minSdk = flutter.minSdkVersion
-//        targetSdk = flutter.targetSdkVersion
-//        versionCode = flutter.versionCode
-//        versionName = flutter.versionName
-//    }
-//
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_11
-//        targetCompatibility = JavaVersion.VERSION_11
-//    }
-//
-//    kotlinOptions {
-//        jvmTarget = "11"
-//    }
-//
-//    buildTypes {
-//        release {
-//            isMinifyEnabled = false
-//            isShrinkResources = false
-//            signingConfig = signingConfigs.getByName("debug")
-//        }
-//    }
-//}
-//
-//flutter {
-//    source = "../.."
-//}
-//
-//dependencies {
-//    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-//    implementation("com.google.firebase:firebase-analytics")
-//}
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.ant.company"
-    compileSdk = 36
-
-    defaultConfig {
-        applicationId = "com.ant.company"
-        minSdk = flutter.minSdkVersion
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
+    namespace = "com.example.kkiri"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -69,17 +16,24 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.kkiri"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            isShrinkResources = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -87,10 +41,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
-    implementation("com.google.firebase:firebase-analytics")
-
 }
