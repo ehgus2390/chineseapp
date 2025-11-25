a3ec7335bfbbbce2035b2b16cd2dbd2b4a63e2c9
ehgus2390 <165624422+ehgus2390@users.noreply.github.com>
2025-11-25 15:12:53 +0900

    qweqwe
    qweqwe

--- kkiri/android/app/build.gradle.kts
+++ kkiri/android/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ant.company"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
